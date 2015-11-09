f1f1d9afb4c72ce2494bce12e677c3aefef92528
Georgy Marrero <georgymarrero@gmail.com>
2015-11-08 19:28:30 -0800

    Remove Parse key from repo
    And of course, reset the keys ;)

--- parse.php
+++ parse.php
@@ -3,9 +3,7 @@
 require 'vendor/autoload.php';
 session_start();
 use Parse\ParseClient;
-ParseClient::initialize('ejDpZA5bF1CSLb7rN18isGvFMGRcrtVa0rQnPKft', 'KVPRkOYsVHGMBg1xES3UKKYU2B0epn3HwHHAYgf5', 'mqnVFUbVdsuOqZLx701NM8AHJQRSGj8XQ9G8TCsl');
-
-
+//ParseClient::initialize('here', 'go', 'keys');
 define('URL', 'localhost/citrus/');
 
  ?>
